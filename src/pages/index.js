6fc32e44c8ea217e67594b65f405b07d65122c1f
Christopher J <chrisbjones1987@yahoo.com>
2023-06-20 15:31:55 -0500

    Added Meme Generator Project Using Next.JS

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import "/src/pages/_app";
+
+function Page(){
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+export default Page;
