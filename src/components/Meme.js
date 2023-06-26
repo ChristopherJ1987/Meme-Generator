dbe39a633c5f682a4b9c6eeee0d78c92e360e213
Christopher J <chrisbjones1987@yahoo.com>
2023-06-26 15:23:49 -0500

    Updated Meme Component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,12 +2,22 @@ import { useState } from "react";
 import memesData from "/src/memesData";
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = useState("");
+    // const [memeImage, setMemeImage] = useState("");
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        setMemeImage(memesArray[randomNumber].url);
+        const url = memesArray[randomNumber].url;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
 
     return (
@@ -25,7 +35,7 @@ export default function Meme() {
                 />
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
-            <img src={memeImage} className="meme--image" />
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
